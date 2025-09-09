74f2f94088fd11df7835289062df4d26fd9f190c
arthurcaraujo <arthur-lagamar@hotmail.com>
2025-09-09 19:59:41 -0300

    chore: Centralize type exports for product, cart, and order

---
+++ src/types/index.ts
@@ -0,0 +1,3 @@
+export type { Product } from "../features/products/productsSlice";
+export type { CartItem } from "../features/cart/cartSlice";
+export type { Order } from "../features/orders/ordersSlice";
